fc64ad033d46648b17054038e06d1395b0735f6f
Janis Hutz <development@janishutz.com>
2026-07-10 18:44:19 +0200

    feat: pull in user association

---
+++ scripts/query-association.sql
@@ -0,0 +1,3 @@
+SELECT d.user_id as user_id, d.furhter_options as cohort, d.survey_id as surveyid
+FROM (((annotator NATURAL JOIN app_user) users JOIN survey_users su ON su.users_id = users.user_id) u JOIN survey s ON u.survey_id = s.id) d
+WHERE d.title <> 'testing' AND d.furhter_options <> '';
